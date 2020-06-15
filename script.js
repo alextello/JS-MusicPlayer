fe700c2b42a843c435f3b730cf86dce0dc2c3279
unknown <alextello1@hotmail.com>
2020-06-15 15:51:08 -0600

    toques finales

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const nextBtn = document.getElementById('next');
 
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
-const progresContainer = document.getElementById('progress-container');
+const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
@@ -20,7 +20,7 @@ loadSong(songs[songIndex]);
 function loadSong(song) {
     title.innerText = song;
     audio.src = `music/${song}.mp3`;
-    cover.src = `images/pinkFloyd.jpg`;
+    cover.src = `images/${song}.jpg`;
 }
 
 // Reproducir cancion
@@ -41,6 +41,48 @@ function pauseSong() {
     audio.pause();
 }
 
+// Canción previa
+
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Siguiente canción
+
+function nextSong() {
+    songIndex++
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Actualizar progreso de la barra
+function setProgress(e) {
+    const width = this.clientWidth;
+    // La posición donde se hizo click en la barra
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Actualizar barra de progreso
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progresoPorcentaje = (currentTime / duration) * 100;
+    progress.style.width = `${progresoPorcentaje}%`
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -51,3 +93,19 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+// Cambiar canción
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Actualiza tiempo de canción
+
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click en la barra de progreso
+
+progressContainer.addEventListener('click', setProgress);
+
+// Cuando termina la canción
+
+audio.addEventListener('ended', nextSong);
